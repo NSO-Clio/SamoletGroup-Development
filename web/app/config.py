8659b4cd150ab1be07e102d985535e44e69cf73a
Vadim Vetrov <vetrovvd@gmail.com>
2024-09-13 20:45:42 +0300

    Allow to download the result and persist the data over sessions via tickets

--- web/app/config.py
+++ web/app/config.py
@@ -3,3 +3,21 @@ from flask import Flask
 def validate_config(app: Flask):
     app.config.from_prefixed_env()
     app.config["MODEL_HOST"]
+
+def validate_cache_config(app: Flask):
+    if "REDIS_CONNECTION_URL" in app.config:
+        print("Using Redis cache")
+        cache_config = {
+            "CACHE_TYPE": "RedisCache",
+            "CACHE_REDIS_URL": app.config["REDIS_CONNECTION_URL"]
+        }
+
+        app.config.from_mapping(cache_config)
+    else:
+        print("Using in-memory cache")
+        cache_config = {
+            "CACHE_TYPE": "SimpleCache"
+        }
+
+        app.config.from_mapping(cache_config)
+    
--- web/app/main.py
+++ web/app/main.py
@@ -1,13 +1,20 @@
 import pandas as pd
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, send_file, url_for, flash
+from flask_caching import Cache
 import requests
+from uuid import uuid4
+import io
 
 import dotenv
 dotenv.load_dotenv()
 from . import config
 
+
 app = Flask(__name__)
 config.validate_config(app)
+config.validate_cache_config(app)
+
+cache = Cache(app)
 
 @app.route('/')
 def index():
@@ -31,16 +38,10 @@ def upload_file():
         resp.raise_for_status()
 
         data = resp.json()
-        def normalize_data(x):
-            s = x['report_date']
-            x['report_date'] = s[8:10] + '.' + s[5:7] + '.' + s[0:4]
-            x['score'] = round(x['score'] * 100)
+        ticket_id = uuid4()
+        cache.set(ticket_id.__str__(), data)
 
-            return x
-
-        data = list(map(normalize_data, data))
-
-        return render_template('table.html', data=data)
+        return redirect(f"/tickets/{ticket_id.__str__()}")
     else:                
         flash('Невозможно обработать файл')
         return redirect(request.url)
@@ -48,3 +49,36 @@ def upload_file():
 @app.get('/upload')
 def upload_view():
         return redirect(url_for('index'))
+
+@app.get('/tickets/<ticket_id>')
+def ticket_view(ticket_id):
+    if not cache.has(ticket_id):
+        return redirect(url_for('index'))
+    data = cache.get(ticket_id)
+
+    def normalize_data(x):
+        s = x['report_date']
+        x['report_date'] = s[8:10] + '.' + s[5:7] + '.' + s[0:4]
+        x['score'] = round(x['score'] * 100)
+
+        return x
+
+    data = list(map(normalize_data, data))
+
+    return render_template('table.html', ticket_id=ticket_id, data=data)
+
+@app.get('/tickets/<ticket_id>/csv')
+def ticket_file(ticket_id):
+    if not cache.has(ticket_id):
+        return redirect(url_for('index'))
+    data = cache.get(ticket_id)
+
+    data_df = pd.DataFrame(data)
+    buf = io.BytesIO()
+    data_df.to_csv(buf, index=False)
+    buf.seek(0)
+
+    return send_file(buf, download_name = f"{ticket_id}.csv")
+
+
+
