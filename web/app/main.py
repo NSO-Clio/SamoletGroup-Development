0cc14c49e167b52c77f384a767ff66af57b8fc41
Vadim Vetrov <vetrovvd@gmail.com>
2024-10-22 16:10:30 +0300

    Add visualization for the best submission

--- web/app/main.py
+++ web/app/main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import numpy as np
 from flask import Flask, render_template, request, redirect, send_file, url_for, flash
 from flask_caching import Cache
 import requests
 from uuid import uuid4
 import io
+from pathlib import Path
+import os
 
 import dotenv
 dotenv.load_dotenv()
@@ -15,6 +18,19 @@ config.validate_config(app)
 config.validate_cache_config(app)
 
 cache = Cache(app)
+
+# set default file
+def load_default_submission():
+    try:
+        df = pd.read_csv(Path(Path(__file__).parent, "./static/submission.csv"))
+        data = list(df.transpose().to_dict().values())
+    except Exception as ex:
+        print(f"Exception while loading default submission: {ex}")
+        data = None
+
+    cache.set("default", data)
+
+    return data
 
 @app.route('/')
 def index():
@@ -50,16 +66,32 @@ def upload_file():
 def upload_view():
         return redirect(url_for('index'))
 
+@app.get('/default')
+def default_view():
+    return redirect(url_for('ticket_view', ticket_id='default'))
+
+def load_cached_ticket(ticket_id):
+    if not cache.has(ticket_id):
+        if ticket_id == 'default':
+            return load_default_submission()
+        else:
+            return None
+
+    return cache.get(ticket_id)
+
+
 @app.get('/tickets/<ticket_id>')
 def ticket_view(ticket_id):
-    if not cache.has(ticket_id):
+    data = load_cached_ticket(ticket_id)
+    if data is None:
         return redirect(url_for('index'))
-    data = cache.get(ticket_id)
 
     def normalize_data(x):
         s = x['report_date']
         x['report_date'] = s[8:10] + '.' + s[5:7] + '.' + s[0:4]
         x['score'] = round(x['score'] * 100)
+        x['contract_id'] = int(x['contract_id'])
+        
 
         return x
 
@@ -69,9 +101,9 @@ def ticket_view(ticket_id):
 
 @app.get('/tickets/<ticket_id>/csv')
 def ticket_file(ticket_id):
-    if not cache.has(ticket_id):
+    data = load_cached_ticket(ticket_id)
+    if data is None:
         return redirect(url_for('index'))
-    data = cache.get(ticket_id)
 
     data_df = pd.DataFrame(data)
     buf = io.BytesIO()
